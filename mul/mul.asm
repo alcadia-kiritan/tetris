e019f7a0530a4aa24764882e388b27e8cbc53277
alcadia kiritan <brownie19870327@gmail.com>
2023-01-18 16:40:22 +0900

    コメント修正

--- mul/mul.asm
+++ mul/mul.asm
@@ -8,18 +8,18 @@ mul8:
     strz r3     ;r3 = r0
     eorz r0     ;r2 = r0 = 0
     strz r2     
-    addz r0      ;Cをリセット, cpslより速いし小さい
+    addz r0     ;キャリーをリセット, cpslより速いし小さい
 
     ppsl WC  ;WCをセット. シフトでのキャリーをありにする
 
-    ;r2:r0(をシフトしたもの) にr3を足していく
+    ;r0:r2(をシフトしたもの) にr3を足していく
     
     tmi,r1 00000001b
     bcfr,eq _m8_0    
     addz r3
 _m8_0:
-    rrr,r0
-    rrr,r2
+    rrr,r0      ;結果の右シフト＆addzでのオーバーフローを最上位ビットへ取り込む
+    rrr,r2      ;結果の右シフト＆キャリーへの０セット
 
     tmi,r1 00000010b
     bcfr,eq _m8_1
@@ -63,7 +63,7 @@ _m8_6:
     rrr,r0
     rrr,r2
 
-    andi,r1 10000000b
+    andi,r1 10000000b   ;tmiよりandiのが速い, 最後なのでr1が破壊されてもOK
     bcfr,lt _m8_7
     addz r3
 _m8_7:
