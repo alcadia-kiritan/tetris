451e8b603331b6aac8c470a1de4b39376d129a9e
alcadia kiritan <brownie19870327@gmail.com>
2023-03-29 01:20:00 +0900

    縦方向のスクロール位置を修正(AnaloguePocketで見切れてた

--- tetris/values.h
+++ tetris/values.h
@@ -215,7 +215,7 @@
     ;------
     ;ここから下画面関係
 
-    SCROLL_Y            equ 222                             ;スクロール位置
+    SCROLL_Y            equ 238                             ;スクロール位置
     SPRITE_OFFSET_Y     equ (SCROLL_Y*3+42-624+24-39)/3     ;描画領域の一番下の行に合う位置
     SPRITE_OFFSET_X     equ (156+102)/6                     ;描画領域の一番左の列に合う位置
 
